276b03e08a7fe21988b801d9ba30d86f5630959b
Otis Singfield <singfieldotis@gmail.com>
2021-08-12 03:52:55 -0400

    completed dynamodb setup

--- .history/server/db/CreateThoughtsTable_20210812033654.js
+++ .history/server/db/CreateThoughtsTable_20210812033654.js
@@ -2,7 +2,7 @@ const AWS = require("aws-sdk");
 
 AWS.config.update({
   region: "us-east-2",
-  // endpoint: "http://localhost:8000",
+  endpoint: "http://localhost:8000",
 });
 
 const dynamodb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
---
+++ .history/server/db/LoadThoughts_20210812032504.js
@@ -0,0 +1,13 @@
+const AWS = require("aws-sdk");
+const fs = require("fs");
+
+AWS.config.update({
+  region: "us-east-2",
+  // endpoint: "http://localhost:8000"
+});
+const dynamodb = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" });
+
+console.log("Importing thoughts into DynamoDB. Please wait.");
+const allUsers = JSON.parse(
+  fs.readFileSync("./server/seed/users.json", "utf8")
+);
